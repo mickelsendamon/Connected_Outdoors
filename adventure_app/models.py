2de1e8d1cddcbc4f02ecb46aa0630bee891387d7
Matyas <csmatthew72@gmail.com>
2021-05-01 14:37:36 -0700

    updated user

--- adventure_app/models.py
+++ adventure_app/models.py
@@ -65,8 +65,8 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    objects = UserManager()
     user_type = models.CharField(max_length=20, default="visitor")
+    objects = UserManager()
 
 
 class Activity(models.Model):
